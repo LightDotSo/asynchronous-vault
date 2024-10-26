3079aba86fec3acd74365fb345daa76658079998
Shun Kakinoki <shunkakinoki@gmail.com>
2024-10-27 00:26:16 +0900

    chore: remove unused

--- src/VaultZapper.sol
+++ src/VaultZapper.sol
@@ -10,7 +10,7 @@ import { SafeERC20 } from
     "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { IERC7540, IERC4626 } from "./interfaces/IERC7540.sol";
-import { PermitParams, AsyncVault } from "./AsyncVault.sol";
+import { PermitParams } from "./AsyncVault.sol";
 import { ERC20Permit } from
     "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
